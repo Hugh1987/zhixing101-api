c91181473857b4e90efc0a1a108740b2ec86e545
周智浩 <mikkomail@163.com>
2016-11-28 20:17:14 +0800

    增加表单内容

--- src/main/java/com/zhixing101/wechat/api/entity/req/CreateBookStoragePlaceRequest.java
+++ src/main/java/com/zhixing101/wechat/api/entity/req/CreateBookStoragePlaceRequest.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * 创建存书点请求
  *
  */
-public class CreateBookStoragePlaceRequest implements Serializable{
+public class CreateBookStoragePlaceRequest implements Serializable {
 
     /**
      * 
@@ -39,9 +39,49 @@ public class CreateBookStoragePlaceRequest implements Serializable{
     private String latitude;
 
     /**
+     * 省
+     */
+    private String province;
+
+    /**
+     * 市
+     */
+    private String city;
+
+    /**
+     * 区
+     */
+    private String district;
+
+    /**
+     * 详细地址
+     */
+    private String address;
+
+    /**
+     * 联系电话
+     */
+    private String phone;
+
+    /**
+     * 手机
+     */
+    private String mobilePhone;
+
+    /**
      * 存放点所有人微信OpenID
      */
     private String wechatOpenId;
+
+    /**
+     * 开放时间
+     */
+    private String openTime;
+
+    /**
+     * 交通
+     */
+    private String traffic;
 
     public String getPoiId() {
         return poiId;
@@ -83,6 +123,54 @@ public class CreateBookStoragePlaceRequest implements Serializable{
         this.latitude = latitude;
     }
 
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
     public String getWechatOpenId() {
         return wechatOpenId;
     }
@@ -91,9 +179,29 @@ public class CreateBookStoragePlaceRequest implements Serializable{
         this.wechatOpenId = wechatOpenId;
     }
 
+    public String getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(String openTime) {
+        this.openTime = openTime;
+    }
+
+    public String getTraffic() {
+        return traffic;
+    }
+
+    public void setTraffic(String traffic) {
+        this.traffic = traffic;
+    }
+
     @Override
     public String toString() {
         return "CreateBookStoragePlaceRequest [poiId=" + poiId + ", name=" + name + ", description=" + description
-                + ", longtitude=" + longtitude + ", latitude=" + latitude + ", wechatOpenId=" + wechatOpenId + "]";
+                + ", longtitude=" + longtitude + ", latitude=" + latitude + ", province=" + province + ", city=" + city
+                + ", district=" + district + ", address=" + address + ", phone=" + phone + ", mobilePhone="
+                + mobilePhone + ", wechatOpenId=" + wechatOpenId + ", openTime=" + openTime + ", traffic=" + traffic
+                + "]";
     }
+
 }
